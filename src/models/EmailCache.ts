1627e43256c587ff1a2210ad5893cd5b92656053
aadv1k <aadv1k@outlook.com>
2023-05-11 15:18:29 +0530

    Setup redis caching logic for email validation

---
+++ src/models/EmailCache.ts
@@ -0,0 +1,43 @@
+import { createClient } from "redis";
+
+class EmailCache {
+    client: any;
+
+    constructor() {
+        this.client = createClient({
+	    socket:{
+		host: "localhost",
+		port: 6379,
+	    }
+	});
+    }
+
+    async init() {
+	await this.client.connect();
+    }
+
+    async pushEmail(email: string, validity: Array<boolean>): Promise<string> {
+	try {
+	    const resp = await this.client.set(email, JSON.stringify(validity));
+	    return email;
+	} catch (err) {
+	    throw err;
+	}
+    }
+
+    async getEmail(email: string): Promise<Array<boolean> | null> {
+	try {
+	    const resp = await this.client.get(email)
+	    return JSON.parse(resp ?? "[]");
+	} catch (err) {
+	    throw err;
+	}
+    }
+
+    async close() {
+	this.client.close();
+    }
+}
+
+export default new EmailCache();
+
