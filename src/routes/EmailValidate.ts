2edaa73093a2d3058669187c284c09aa40fe8184
aadv1k <aadv1k@outlook.com>
2023-05-11 17:37:35 +0530

    Fix and refactor code

--- src/routes/EmailValidate.ts
+++ src/routes/EmailValidate.ts
@@ -1,6 +1,6 @@
 import Express from "express";
 import { ErrorCodes } from "../const/errors";
-import { sendErrorResponse, sendJSONResponse } from "../utils";
+import { sendErrorResponse, sendJSONResponse, isURL } from "../utils";
 import dns from "node:dns";
 
 import EmailCache from "../models/EmailCache";
@@ -15,68 +15,65 @@ function validateEmailSyntax(email: string): boolean {
   }
   const [localPart, domain] = email.split("@");
 
-  if (localPart.length > maxLocalPartLength || domain.length > maxDomainLength) {
+  if (localPart?.length > maxLocalPartLength || domain?.length > maxDomainLength) {
     return false;
   }
   return regex.test(email);
 }
 
-function validateEmailDomain(email: string): Promise<Array<string> | null> {
-    const [mail, tld] = email.split("@");
+function validateEmailDomain(email: string): Promise<boolean>  {
+		const [mail, tld] = email.split("@");
+		if (!tld || !isURL(tld)) Promise.resolve(false);
 
-    return new Promise((resolve, reject) => {
-	dns.resolve(tld, "MX", (err, addresses) => {
-	    if (err) reject(null);
-	    resolve(addresses?.map(e => e.exchange) ?? [])
-	})
-    })
+		return new Promise((resolve, reject) => {
+				dns.resolve(tld, "MX", (err, addresses) => {
+						if (err) resolve(false);
+						resolve(addresses?.length > 0)
+				})
+		})
 }
 
 export default async function (req: Express.Request, res: Express.Response) {
-    const queryEmail = req.query?.email;
+		const queryEmail = req.query?.email;
 
-    if (!queryEmail) {
-	sendErrorResponse(res, {
-	    error: {
-		code: ErrorCodes.BAD_INPUT,
-		message: "email is a required argument",
-	    },
-	    status: 400
-	});
-	return;
-    }
+		if (!queryEmail) {
+				sendErrorResponse(res, {
+						error: {
+								code: ErrorCodes.BAD_INPUT,
+								message: "email is a required argument",
+						},
+						status: 400
+				});
+				return;
+		}
 
-    const cachedEmailData = await EmailCache.getEmail(queryEmail as string);
+		const cachedEmailData = await EmailCache.getEmail(queryEmail as string);
 
+		let emailChecks;
+		if (cachedEmailData?.length > 0) {
+				emailChecks = cachedEmailData;
+		} else {
+				emailChecks = [
+						validateEmailSyntax(queryEmail as string),
+						await validateEmailDomain(queryEmail as string)
+				]
+		}
 
-    let emailChecks;
+		emailChecks = emailChecks as Array<boolean>;
 
-    if (cachedEmailData?.length) {
-	console.log("found: ", queryEmail, cachedEmailData);
-	emailChecks = cachedEmailData;
-    } else {
-	emailChecks = [
-	    validateEmailSyntax(queryEmail as string),
-	    await validateEmailDomain(queryEmail as string),
-	]
-    }
+		const response = {
+				data: {
+						email: queryEmail as string,
+						valid: emailChecks.every(e => e),
+						format_valid: emailChecks[0],
+						domain_valid: emailChecks[1],
+				},
+				status: 200
+		}
 
+		if (cachedEmailData.length < 0) {
+				await EmailCache.pushEmail(queryEmail as string, emailChecks as Array<boolean>);
+		}
 
-    emailChecks = emailChecks as Array<boolean>;
-    const response = {
-	data: {
-	    email: queryEmail as string,
-	    valid: emailChecks.every(e => e),
-	    format_valid: emailChecks[0],
-	    domain_valid: Boolean(emailChecks[1]),
-	    dns_records: emailChecks[1] || [],
-	},
-	status: 200
-    }
-
-    if (!cachedEmailData?.length) {
-	console.log("cached", queryEmail);
-	await EmailCache.pushEmail(queryEmail as string, emailChecks as Array<boolean>);
-    }
-    sendJSONResponse(res, response)
+		sendJSONResponse(res, response)
 }
