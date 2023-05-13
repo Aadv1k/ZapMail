6f075b22d97cc3c52c8e4038e9ee279691b9db9f
aadv1k <aadv1k@outlook.com>
2023-05-13 16:24:14 +0530

    Add build commands and prepare for deploy

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import server from "./server";
+const PORT = process.env.PORT || 3000;
 
-server.listen(3000, () => {
-    console.log("server listening at http://127.0.0.1:3000")
+server.listen(PORT, () => {
+    console.log(`server listening at http://127.0.0.1:${PORT}`)
 })
